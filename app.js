d96bd4295ba31e8266ebf0c091809401d5a9eece
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2021-12-26 13:22:11 +0000

    Added Styles file and First round of Play

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
 let gameOptions = ['Rock','Paper','Scissors'];
+let getName = prompt('Kindly Enter your Name');
+let playerName = document.getElementById('player-name');
+let playersName = document.getElementById('players-name');
+
 let computerChoiceDisplay = document.getElementById('computer-choice');
 let userChoiceDisplay = document.getElementById('user-choice');
+let userScore = document.getElementById('user-score');
+let computerScore = document.getElementById('computer-score');
 let gameResult = document.getElementById('result');
 let buttonEvent = document.querySelectorAll('button');
 let computerSelect 
 let clickedButton
-const computerSelection = computerSelect;
-const playerSelection = clickedButton;
+let computerSelection
+let playerSelection
+
+let playerScore = 0;
+let compScore = 0;
+
+userScore.innerHTML = playerScore;
+computerScore.innerHTML = compScore;
+playerName.innerHTML = getName;
+playersName.innerHTML = getName;
 
 // computer selection
 function computerPlay() {
@@ -19,31 +33,70 @@ function computerPlay() {
   ];
 
   computerChoiceDisplay.innerHTML = computerSelect;
+  computerSelection = computerSelect;
+  
+  // console.log(computerSelection);
+
 
 };
 
 
 // User selection
+for ( var i=0; i < buttonEvent.length; i++){
+  buttonEvent[i].addEventListener('click', handleClick);
+}
 
-// function userPlay(){
-  buttonEvent.forEach(
-    function(){
-      this.addEventListener('click',function (e){
+function handleClick (e){
         clickedButton = e.target.id;
         userChoiceDisplay.innerHTML = clickedButton;
+        playerSelection = clickedButton;
         computerPlay();
-        console.log(e.target.id)
-        // console.log(computerSelection);
-      })
-    }
-  )
+        playRound(playerSelection,computerSelection);
+        
+
+  };
 // }
 
 function playRound(playerSelection,computerSelection){
   if (playerSelection === computerSelection){
-    gameResult.innerHTML = 'It\'s a Tie!'
+    gameResult.innerHTML = 'It\'s a Tie!';
   }
+  
+  if(
+      (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+      (playerSelection === 'Scissors' && computerSelection === 'Paper') || 
+      (playerSelection === 'Paper' && computerSelection === 'Rock')
+    ){
+    gameResult.innerHTML = getName + ' Wins!';
+    playerScore++;
+    userScore.innerHTML = playerScore;
+    computerScore.innerHTML = compScore;
+  }
+
+  if(
+    (playerSelection === 'Rock' && computerSelection === 'Paper') || 
+    (playerSelection === 'Scissors' && computerSelection === 'Rock') || 
+    (playerSelection === 'Paper' && computerSelection === 'Scissors')
+  ){
+    gameResult.innerHTML ='Computer Wins!';
+    compScore++;
+    userScore.innerHTML = playerScore;
+    computerScore.innerHTML = compScore;
+  }
+  
+  if(playerScore === 5){
+
+    gameResult.innerHTML = getName + ' Wins 5 points!';
+
+  } else if( compScore === 5 ){
+    gameResult.innerHTML ='Computer Wins 5 Points!';
+
+  }
+  
 }
+
+
+
 
 
 
